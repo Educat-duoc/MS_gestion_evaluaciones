f65151fa16d077b9982a531b9268e70cc3fcd730
francis oses <fra.oses@duocuc.cl>
2025-05-22 13:59:52 -0400

    agrege el metodo findByMateria en EvaluacionRepository para buscar evaluaciones por materia

--- ms_gestion_evaluaciones/src/main/java/cl/duoc/ms_gestion_evaluaciones/repository/EvaluacionRepository.java
+++ ms_gestion_evaluaciones/src/main/java/cl/duoc/ms_gestion_evaluaciones/repository/EvaluacionRepository.java
@@ -1,5 +1,12 @@
 package cl.duoc.ms_gestion_evaluaciones.repository;
 
-public interface EvaluacionRepository {
+import cl.duoc.ms_gestion_evaluaciones.model.Evaluacion;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EvaluacionRepository extends JpaRepository<Evaluacion, Long> {
+    List<Evaluacion>findByMateria(String materia);
 }
