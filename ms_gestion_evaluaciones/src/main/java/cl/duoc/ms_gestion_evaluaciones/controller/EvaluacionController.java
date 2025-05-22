34eae13cce0dd82621c72ba4f49da89ea36830cd
francis oses <fra.oses@duocuc.cl>
2025-05-22 14:05:43 -0400

    Probando

--- ms_gestion_evaluaciones/src/main/java/cl/duoc/ms_gestion_evaluaciones/controller/EvaluacionController.java
+++ ms_gestion_evaluaciones/src/main/java/cl/duoc/ms_gestion_evaluaciones/controller/EvaluacionController.java
@@ -3,17 +3,27 @@ package cl.duoc.ms_gestion_evaluaciones.controller;
 import cl.duoc.ms_gestion_evaluaciones.model.Evaluacion;
 import cl.duoc.ms_gestion_evaluaciones.service.EvaluacionService;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+/*import org.springframework.web.bind.annotation.PostMapping;*/
+/*import org.springframework.web.bind.annotation.RequestBody;*/
+/*import org.springframework.web.bind.annotation.PutMapping;*/
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+@RestController
+@RequestMapping("/Evaluacion")
+@RequiredArgsConstructor
 
 public class EvaluacionController {
 
